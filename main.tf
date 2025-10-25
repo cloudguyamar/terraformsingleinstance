54b55ecc20369b12d7cb14fde8faeefd4ba897a1
cloudguyamar <cloudguyamar@gmail.com>
2025-10-25 21:30:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,8 +104,8 @@ resource "aws_security_group" "allow_all" {
 
 # data "aws_ami" "my_ami" {
 #      most_recent      = true
-#      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
+#      #name_regex       = "Cloudguyamar"
+#      owners           = ["1111111111"]
 # }
 
 
@@ -115,14 +115,14 @@ resource "aws_security_group" "allow_all" {
 #     #ami = "${data.aws_ami.my_ami.id}"
 #     availability_zone = "us-east-1a"
 #     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
+#     key_name = "testKey"
 #     subnet_id = "${aws_subnet.subnet1-public.id}"
 #     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
 #     associate_public_ip_address = true	
 #     tags = {
 #         Name = "Server-1"
 #         Env = "Prod"
-#         Owner = "Sree"
+#         Owner = "amar"
 # 	CostCenter = "ABCD"
 #     }
 # }
